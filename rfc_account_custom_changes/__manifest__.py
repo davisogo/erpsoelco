832a35e9f795c52092ff6ef178080ff63222a4a8
odooco <odoofriend@gmail.com>
2022-03-31 10:17:52 -0500

    [ADD]account_custom_report

---
+++ rfc_account_custom_changes/__manifest__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': "RFC Account Custom",
+    'version': '1.0',
+    'description': """This will customize the Aged Reveivable and Aged Payable in Accounting Coondev """,
+    'license': '',
+    'category':'Accounting',
+    'depends': ['base','account_accountant','account_reports'],
+    'data': [],
+    'installable': True,
+    'application': True,
+    'auto_install':False,
+}
